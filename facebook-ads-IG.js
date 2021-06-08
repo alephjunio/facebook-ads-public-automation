7bd82092be265797bd0dc8842f6a45d10acd608a
Felipe Almeman <felipe@aireset.com.br>
2021-06-08 01:18:12 -0300

    Update Facebook Ads - IG public

--- facebook-ads-IG.js
+++ facebook-ads-IG.js
@@ -1,12 +1,41 @@
-var business_id = 0000000000000000; // pegar na página que está logado do adsmanager/audience
-var page_id = "000000000000000"; // precisa ser uma string
-var token = "aaaaaaaaaaaaa"; // pegar na página que está logado do adsmanager/audience
-var action = 000000000000000; // pegar na página que está logado do adsmanager/audience
-var sessionID = "0000aa000a0a000"; // pegar na página que está logado do adsmanager/audience
+// Tipos de regras simples
+var types = [
+    [
+        "ig_business_profile_all", // instagram - Todos que se engajaram com sua conta profissional
+        '[social][ig][todos]',
+        "43"
+    ],
+    [
+        "ig_business_profile_visited", // instagram - Qualquer pessoa que visitou o perfil da sua conta profissional
+        '[social][ig][visitaram]',
+        "43"
+    ],
+    [
+        "ig_business_profile_engaged", // instagram - Pessoas que se engajaram com qualquer publicação ou anúncio
+        '[social][ig][engajaram]',
+        "43"
+    ],
+    [
+        "ig_business_profile_user_messaged", // instagram - Pessoas que enviaram uma mensagem para a sua conta profissioal
+        '[social][ig][mensagem]',
+        "43"
+    ],
+    [
+        "ig_business_profile_ad_saved", // instagram - Pessoas que salvaram qualquer publicação ou anúncio
+        '[social][ig][salvaram]',
+        "43"
+    ],
+];
+
+var business_id = 0000000000000000;
+var ig_id = "coloque o id da conta do instagram aqui"; // precisa ser uma string
+var token = "coloque seu token aqui"; // pegar na página que está logado do adsmanager/audience
+var action = 000000000000000;
+var sessionID = "coloque o id da session aqui"; // pegar na página que está logado do adsmanager/audience
 var seconds = 24 * 60 * 60;
 
 function fetchBody(body) {
-    fetch("https://graph.facebook.com/v10.0/act_311377643156915/customaudiences?_app=ADS_MANAGER&_reqName=adaccount%2Fcustomaudiences&access_token=EAABsbCS1iHgBAMxnP28DVkh5pLvxtW7coMUyQgzv91BXHz2RZBFZAsKfZCnuSyxknAsRRQC1qGN0KPFZAt78N2Mj4V2ta72TydZAgacZBZACGyxFROxxhID68TN3ZCqnewfMZCvBfLh8jEaWVBuZCxQdRXELJaM6sdTuozTvUOBaHpDerWBm15XZBdeRpGDKDH04kYZD&method=post&__cppo=1", {
+    fetch("https://graph.facebook.com/v10.0/act_" + action + "/customaudiences?_app=ADS_MANAGER&_reqName=adaccount%2Fcustomaudiences&access_token=" + token + "&method=post&__cppo=1", {
         "headers": {
             "accept": "*/*",
             "accept-language": "pt-BR,pt;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
@@ -27,10 +56,18 @@ function fetchBody(body) {
     });
 }
 
-var types = [
-    "ig_business_profile_all",
-];
-var type = type[0];
+var masterType = types[4]; // Seleciona o Master Type
+var type = masterType[0]
+var textSeparator = masterType[1];
+var indexType = masterType[2];
+
+// console.log('masterType');
+// console.log(masterType);
+// console.log('type');
+// console.log(type);
+// console.log('textSeparator');
+// console.log(textSeparator);
+
 var days = [
     1,
     2,
@@ -46,23 +83,41 @@ var days = [
 ];
 days.forEach(function (day) {
     var dayPad = String(day).padStart(3, '0');
-    let bodyIG = {
+
+    let textName = textSeparator;
+    if (type != 'page_liked') {
+        textName += " " + dayPad + "d";
+    }
+
+    let body = {
         "__activeScenarioIDs": "[\"f6af8df90cebc4_1623115007094.8\"]",
         "__activeScenarios": "[\"am.edit_targeting.create_ig_ca\"]",
-        "__business_id": "1920505904742398",
+        "__business_id": business_id,
         "_app": "ADS_MANAGER",
-        "_index": "43",
+        "_index": indexType,
         "_reqName": "adaccount/customaudiences",
         "_reqSrc": "AdsAMAudienceGraphAPI",
-        "_sessionID": "3a842b2c2e76cbd6",
+        "_sessionID": sessionID,
         "description": "",
         "include_headers": "false",
         "locale": "pt_BR",
         "method": "post",
-        "name": "[social][fb][todos] " + dayPad + "d",
+        "name": textName,
         "prefill": "true",
         "pretty": "0",
-        "rule": "{\"inclusions\":{\"operator\":\"or\",\"rules\":[{\"event_sources\":[{\"type\":\"ig_business\",\"id\":\"1874141355956494\"}],\"retention_seconds\":" + (day * seconds) + ",\"filter\":{\"operator\":\"and\",\"filters\":[{\"field\":\"event\",\"operator\":\"eq\",\"value\":\"" + type + "\"}]}}]}}",
+        "rule": JSON.stringify({
+            "inclusions": {
+                "operator": "or",
+                "rules": [{
+                    "event_sources": [{"type": "ig_business", "id": ig_id}],
+                    "retention_seconds": (day * seconds),
+                    "filter": {
+                        "operator": "and",
+                        "filters": [{"field": "event", "operator": "eq", "value": type}]
+                    }
+                }]
+            }
+        }),
         "suppress_http_code": "1",
         "xref": "f385a9fb4887664"
     };
@@ -70,6 +125,6 @@ days.forEach(function (day) {
         return encodeURIComponent(key) + '=' + encodeURIComponent(body[key])
     }).join('&');
 
-    console.log(queryString)
-//   fetchBody(queryString);
+    // console.log(queryString)
+    fetchBody(queryString);
 })
